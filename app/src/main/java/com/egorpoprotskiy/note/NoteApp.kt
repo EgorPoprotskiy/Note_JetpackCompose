469b3b781a05f0d4e4a07a3dd44700f854356ce3
EgorPoprotskiy <egorpoprotskiy@gmail.com>
2025-06-05 01:50:57 +1000

    2_add_Note_and_NoteApp

---
+++ app/src/main/java/com/egorpoprotskiy/note/NoteApp.kt
@@ -0,0 +1,54 @@
+package com.egorpoprotskiy.note
+
+import android.R
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+
+import com.egorpoprotskiy.note.R.string
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NoteApp(navController: NavHostController = rememberNavController()) {
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NoteTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(title)
+        },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(
+                    onClick = navigateUp
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(com.egorpoprotskiy.note.R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
